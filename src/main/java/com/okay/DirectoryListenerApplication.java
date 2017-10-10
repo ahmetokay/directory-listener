2d30e56422596556fcd437a252a5db6f4dca3984
Ahmet Okay <ahmet.okay@outlook.com>
2017-10-10 10:56:49 +0300

    Add error log while registering directory.

--- src/main/java/com/okay/DirectoryListenerApplication.java
+++ src/main/java/com/okay/DirectoryListenerApplication.java
@@ -1,6 +1,8 @@
 package com.okay;
 
 import com.okay.manager.DirectoryManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,16 +20,16 @@ import java.io.IOException;
 @EnableScheduling
 public class DirectoryListenerApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(DirectoryListenerApplication.class);
+
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(DirectoryListenerApplication.class);
 
         DirectoryManager directoryManager = ctx.getBean(DirectoryManager.class);
         try {
             directoryManager.listenDirectory();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (IOException | InterruptedException e) {
+            logger.error("Error occurred while listening directory", e);
         }
     }
 }
